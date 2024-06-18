45350361816249388e8b91d523203eb9a10dd688
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-18 00:12:16 -0700

    Initialized Home Page

---
+++ src/screens/Home/HomePage.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import "./HomePage.css";
+import SearchOptions from "../../components/SearchOptions/SearchOptions";
+import BoardList from "../../components/BoardList/BoardList";
+
+const HomePage = () => {
+  return (
+    <div className="searchScreen-container">
+      <SearchOptions />
+      <h1 className="searchScreen-title">No Boards have been made</h1>
+      <BoardList />
+    </div>
+  );
+};
+
+export default HomePage;
