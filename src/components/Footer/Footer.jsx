5277b8a7532e94a54e422b2d12ec62c3b16ab499
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-18 00:09:29 -0700

    created footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>All rights reserved</p>
+      <p>Made By Bryan Ansong</p>
+    </div>
+  );
+};
+
+export default Footer;
