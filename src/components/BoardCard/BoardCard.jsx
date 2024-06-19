307b28cd7f964a6f08f06932fd696a0136e5ae01
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-18 19:40:34 -0700

    Designed Board Card

--- src/components/BoardCard/BoardCard.jsx
+++ src/components/BoardCard/BoardCard.jsx
@@ -3,23 +3,19 @@ import "./BoardCard.css";
 const BoardCard = () => {
   return (
     <div
-      className="card"
-      // TODO: add onClick event for opening board creation modal
+      className="board-card"
+      // TODO: handle onClick event for opening board creation modal
       // onClick={() => openModal(movie)}
     >
       <img
-        src={
-          movie?.poster_path
-            ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-            : "https://placehold.co/260x390?text=No+Cover+Art&font=montserrat"
-        }
-        className="movieCover"
-        alt="Movie Cover Art"
+        src={"https://placehold.co/260x390?text=No+Image&font=montserrat"}
+        className="board-img"
+        alt="Kudo Board Cover"
       />
-      <div className="movieText">
-        <h2 className="movieTitle">
-          Kudo Board Name
-        </h2>
+      <div className="board-description">
+
+      <h2 className="board-title">Kudo Board Name</h2>
+      <p className="board-author">Bryan Ansong</p>
       </div>
     </div>
   );
