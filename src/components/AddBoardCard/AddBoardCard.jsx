974754152d4a9892ab4de104b56dacbb692b183e
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-19 11:20:26 -0700

    Created add board card

---
+++ src/components/AddBoardCard/AddBoardCard.jsx
@@ -0,0 +1,13 @@
+import "./AddBoardCard.css";
+
+const AddBoardCard = () => {
+  // TODO:  Should open a modal to add a new board to the list
+
+  return (
+    <div className="add-board">
+      <h3>Create New Board</h3>
+    </div>
+  );
+};
+
+export default AddBoardCard;
