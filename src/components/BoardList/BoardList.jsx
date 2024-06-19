55367f2a8cb779502e14fac1318ccbc3157091bc
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-18 19:54:44 -0700

    Added skeleton of board list

--- src/components/BoardList/BoardList.jsx
+++ src/components/BoardList/BoardList.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import BoardCard from "../BoardCard/BoardCard";
+import "./BoardList.css";
 
 const BoardList = () => {
-  const boardList = useState([]);
+  const [boardList, setBoardList] = useState([]);
+
+  // TODO: Create Fetch function to get all board entries
 
   return (
-      <div className="board-list">
-        {boardList.map((board, index) => (
-          // TODO: Render each Board Ard
-          <></>
-        ))}
-      </div>
+    <div className="board-list">
+      {boardList.map((board, index) => (
+        <div key={index}>
+          <BoardCard board={board} />
+        </div>
+      ))}
+    </div>
   );
 };
 
