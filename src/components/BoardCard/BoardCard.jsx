8b2d4c95b4eac4b2e2956d4c7fc596af2de2921a
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-20 11:09:23 -0700

    Implemented Logic to delete board

--- src/components/BoardCard/BoardCard.jsx
+++ src/components/BoardCard/BoardCard.jsx
@@ -1,14 +1,36 @@
 import "./BoardCard.css";
 
-const BoardCard = ({ board }) => {
+const BoardCard = ({ board, refreshBoardsList }) => {
+  const deleteBoard = () => {
+    fetch(`http://localhost:3000/boards/${board.id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(refreshBoardsList())
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div
       className="board-card"
       // TODO: handle onClick event for opening board page
       // onClick={() => openModal(board)}
     >
+      <div
+        className="delete-board-button"
+        onClick={(e) => {
+          console.log("BOARD DELETED SUCCESSFULLY");
+          e.stopPropagation();
+          deleteBoard();
+        }}
+      >
+        DELETE BOARD
+      </div>
       <img
-        src={board.image || "https://placehold.co/260x390?text=No+Image&font=montserrat"}
+        src={
+          board.image ||
+          "https://placehold.co/260x390?text=No+Image&font=montserrat"
+        }
         className="board-img"
         alt="Kudo Board Cover"
       />
