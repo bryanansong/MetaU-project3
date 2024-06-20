45e7f1a750d40703b8e1709797f0fe3c27ab3e80
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-20 16:52:40 -0700

    Created add kudo card

---
+++ src/components/AddKudoCard/AddKudoCard.jsx
@@ -0,0 +1,11 @@
+import "./AddKudoCard.css";
+
+const AddKudoCard = ({ toggleModal }) => {
+  return (
+    <div className="add-card" onClick={() => {}}>
+      <h3>Create New Kudo</h3>
+    </div>
+  );
+};
+
+export default AddKudoCard;
