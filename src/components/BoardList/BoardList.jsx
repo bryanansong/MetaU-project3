34362eb061a18e880a228ec684415347404a91ff
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-20 01:28:51 -0700

    implemented fetch boards method

--- src/components/BoardList/BoardList.jsx
+++ src/components/BoardList/BoardList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./BoardList.css";
 import BoardCard from "../BoardCard/BoardCard";
 import AddBoardCard from "../AddBoardCard/AddBoardCard";
@@ -11,11 +11,29 @@ const BoardList = () => {
   const closeModal = () => {
     setIsVisible(false);
   };
+
   const openModal = () => {
     setIsVisible(true);
   };
 
   // TODO: Create Fetch function to get all board entries
+  const fetchBoardList = () => {
+    fetch("http://localhost:3000/boards", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjdiNDBjODkyMGJjNzMyODk1MTU0M2IyM2MwMjg3YSIsInN1YiI6IjY2Njc2NDY0YTdmNzVmZDVhODFjMzAyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C6GUy0On998UxcO7SY7sKs6S06ECbxQJ_Vsav1vGusw",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => setBoardList(response))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchBoardList();
+  });
 
   return (
     <div className="board-list">
@@ -26,7 +44,7 @@ const BoardList = () => {
           <BoardCard board={board} />
         </div>
       ))}
-      <AddBoardCard toggleModal={setIsVisible} />
+      <AddBoardCard toggleModal={openModal} />
     </div>
   );
 };
