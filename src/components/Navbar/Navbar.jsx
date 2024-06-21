97ffc7013927119c2c470398c50a81f4b2ef0155
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-21 14:58:37 -0700

    styled nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,39 @@
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const location = useLocation();
+  let navBarTitle;
+
+  if (location.pathname.includes("/board")) {
+    navBarTitle = "Boards Page";
+  } else if (location.pathname.includes("/kudo")) {
+    navBarTitle = "Kudo Card";
+  } else {
+    navBarTitle = "Home";
+  }
+
   return (
     <div className="navbar">
-      <h1 className="navbar-title">Kudo Boards!!</h1>
+      {location.pathname !== "/" && (
+        <Link className="back-button" to={-1}>
+          <svg
+            width="8"
+            height="14"
+            viewBox="0 0 8 14"
+            fill="none"
+          >
+            <path
+              d="M7 13L1 7L7 1"
+              stroke="black"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </Link>
+      )}
+      <h1 className="navbar-title">{navBarTitle}</h1>
     </div>
   );
 };
