f5c8514955ea73f5fa820334e8983e0a4b309e3b
Bryan Ansong <bryanansong2003@gmail.com>
2024-06-20 17:12:59 -0700

    Made data on card dynamic

--- src/components/KudoCard/KudoCard.jsx
+++ src/components/KudoCard/KudoCard.jsx
@@ -1,6 +1,22 @@
 import "./KudoCard.css";
+import { useEffect, useState } from "react";
 
 const KudoCard = ({ kudo }) => {
+  const [creator, setCreator] = useState({});
+
+  const fetchUserData = () => {
+    fetch(`http://localhost:3000/users/${kudo.userId}`)
+      .then((response) => response.json())
+      .then((response) => setCreator(response))
+      .catch((err) => console.error(err));
+  };
+
+  const handleUpvote = () => {};
+  const handleDownvote = () => {};
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
   return (
     <div
       className="kudo-card"
@@ -12,13 +28,27 @@ const KudoCard = ({ kudo }) => {
         className="kudo-img"
         alt="Movie Cover Art"
       />
-      <h2 className="kudo-title">Kudo Card Name</h2>
+      <h2 className="kudo-title">{kudo.title}</h2>
       <div className="kudo-actions">
         <div className="reaction-buttons" onClick={(e) => e.stopPropagation()}>
-          <button className="upvote">⬆️</button>
-          <button className="downvote">⬇️</button>
+          <button
+            className="upvote"
+            onClick={() => {
+              handleUpvote();
+            }}
+          >
+            ⬆️
+          </button>
+          <button
+            className="downvote"
+            onClick={() => {
+              handleDownvote();
+            }}
+          >
+            ⬇️
+          </button>
         </div>
-        <p className="kudo-author">Bryan Ansong</p>
+        <p className="kudo-author">{creator.username}</p>
       </div>
     </div>
   );
